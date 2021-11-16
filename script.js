2574ea0a0bc4e294047cc05f383445bf9ebce415
Slawek Galka <galka.slawek6@gmail.com>
2021-11-16 12:31:37 +0100

    visual apperance

---
+++ script.js
@@ -0,0 +1,12 @@
+class Calculator {
+    constructor(previous)
+}
+
+
+const numberButtons = document.querySelectorAll('.num');
+const operationButtons = document.querySelectorAll('.op');
+const equalButton = document.querySelector('.eq');
+const deleteButton = document.querySelector('.del'); 
+const clearButton = document.querySelector('.cl'); 
+const previousEl = document.querySelector('.prev'); 
+const currentEl = document.querySelector('.curr'); 
